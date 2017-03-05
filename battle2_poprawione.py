8acdeece7465f3a58c23c6f1c234c381950fccdd
stodola <florian.winski@interia.pl>
2017-03-05 12:42:29 +0100

    better battle2_poprawione

--- battle2_poprawione.py
+++ battle2_poprawione.py
@@ -1,23 +1,21 @@
 def battle(soldier_list):
-    even = []
-    odd = []
+    sum_even = 0
+    sum_odd = 0
 
     for i in soldier_list:
         if i == 0:
             pass
         elif i%2 == 0:
             if i < 0:
-                even.append(('min', bin(i)[3:].count('0')))
+                sum_even -=  bin(i)[3:].count('0')
             else:
-                even.append(('plus', bin(i)[2:].count('0')))
+                sum_even += bin(i)[2:].count('0')
         else:
             if i < 0:
-                odd.append(('min', bin(i)[3:].count('1')))
+                sum_odd -= bin(i)[3:].count('1')
             else:
-                odd.append(('plus', bin(i)[2:].count('1')))
+                sum_odd += bin(i)[2:].count('1')
     
-    sum_even = summer(even)
-    sum_odd = summer(odd)
     
     if sum_even > sum_odd:
         return 'evens win'
@@ -26,25 +24,6 @@ def battle(soldier_list):
     else:
         return 'tie'   
   
-
-def summer(list_numbers):
-    ''' This function takes as argument list of tuples i.e. ('min', 7). 
-        First element in tuple is a string which contains information, 
-        whether the second element has to be added to result ('plus') or 
-        subtracted from result ('min'). 
-        Function returns sum of second elements of tuple with proper sign.
-    ''' 
-
-    result = 0
-    for sign, number in list_numbers:
-        if sign == 'min':
-            result -= number
-        else:
-            result += number
-    
-    return result
-
-
 
 
 
@@ -55,7 +34,7 @@ if __name__ == '__main__':
 #    a=[7,-3,-14,6]
 #    a=[17,-3, 32, -24]
 
-    a=[0,1,2]
+    a=[0,1]
     print (battle(a))
 
 
